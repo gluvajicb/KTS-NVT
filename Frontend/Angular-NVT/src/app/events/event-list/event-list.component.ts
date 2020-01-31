f2ac5b05677157bdbb3ad8714f6c038521684a35
Nikolina Petrović <nikolinaapetrovic97@gmail.com>
2020-01-31 16:35:55 +0100

    Event search and filter implemented

--- Frontend/Angular-NVT/src/app/events/event-list/event-list.component.ts
+++ Frontend/Angular-NVT/src/app/events/event-list/event-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input, SimpleChanges} from '@angular/core';
 import { Event } from '../model/event';
 import { EventsService } from '../services/events.service';
 
@@ -12,6 +12,10 @@ export class EventListComponent implements OnInit {
   private currentPage: number;
   private totalSize: number;
   private eventList: Event[];
+  private allEvents: Event[];
+
+  @Input() filterCategory: string;
+  @Input() search: string;
 
   constructor(private eventsService: EventsService) {
     this.pageSize = 8;
@@ -23,7 +27,7 @@ export class EventListComponent implements OnInit {
     this.eventsService.getAll(this.currentPage - 1, this.pageSize)
       .subscribe( res => {
         this.eventList = res.body as Event[];
-        console.log(this.eventList);
+        this.allEvents = this.eventList;
       });
   }
 
@@ -31,4 +35,21 @@ export class EventListComponent implements OnInit {
   deleteEvent(id: number) {
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    let category = this.filterCategory;
+    let searchValue = this.search;
+    console.log(this.filterCategory);
+    console.log(this.allEvents);
+    if (this.filterCategory !== 'ANY' && this.filterCategory !== 'ANY CATEGORY') {
+      this.eventList = this.allEvents.filter(function (el) {
+        return el.eventCategory === category && el.title.toLowerCase().includes(searchValue.toLowerCase());
+      });
+    } else {
+      console.log(searchValue);
+      this.eventList = this.allEvents.filter(function (el) {
+        return el.title.toLowerCase().includes(searchValue.toLowerCase());
+      });
+    }
+    console.log(this.eventList);
+  }
 }
