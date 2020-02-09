33a11f5e915b348044a3b4e3b596ec512cec9b46
Vukasin Petrovic <work@vpetrovic.com>
2020-02-09 03:02:57 +0100

    - Doradjeni testovi za user service

--- KTS_NVT/src/test/java/Tim20/KTS_NVT/service/UserServiceIntegrationTest.java
+++ KTS_NVT/src/test/java/Tim20/KTS_NVT/service/UserServiceIntegrationTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import tim20.KTS_NVT.dto.UserDTO;
+import tim20.KTS_NVT.exceptions.*;
 import tim20.KTS_NVT.model.User;
 import tim20.KTS_NVT.security.UserTokenState;
 import tim20.KTS_NVT.service.UserService;
@@ -31,7 +32,7 @@ public class UserServiceIntegrationTest {
 
     @Test
     public void findById() {
-        long id = 1;
+        long id = 2;
         User user = userService.findById(id);
 
         assertNotNull(user);
@@ -40,7 +41,7 @@ public class UserServiceIntegrationTest {
 
     @Test
     public void findByIdNotFound() {
-        long id = 2;
+        long id = 398;
         User user = userService.findById(id);
 
         assertNull(user);
@@ -54,7 +55,15 @@ public class UserServiceIntegrationTest {
         assertNotNull(token);
     }
 
-    @Test
+    @Test(expected = FieldsRequiredException.class)
+    public void loginNoData() {
+        UserDTO dto = new UserDTO(null, "");
+        UserTokenState token = userService.loginUser(dto);
+
+        assertNotNull(token);
+    }
+
+    @Test(expected = WrongCredentialsException.class)
     public void loginWrongCredentials() {
         UserDTO dto = new UserDTO("perke", "asdfasd");
         UserTokenState token = userService.loginUser(dto);
@@ -71,9 +80,43 @@ public class UserServiceIntegrationTest {
         assertTrue(response);
     }
 
+    @Test(expected = FieldsRequiredException.class)
+    public void registerNoData() {
+        UserDTO dto = new UserDTO("", "", "", "", "", "", "");
+        userService.registerUser(dto);
+    }
+
+    @Test(expected = UsernameTakenException.class)
+    public void registerUsernameTaken() {
+        UserDTO dto = new UserDTO("vpetrovic", "test123", "test123", "Petar", "Markovic", "perica@gmail.com", "065 353 97 23");
+        userService.registerUser(dto);
+    }
+
+    @Test(expected = PasswordsNotMatchingException.class)
+    public void registerPasswordsDontMatch() {
+        UserDTO dto = new UserDTO("perica", "test123", "Test123", "Petar", "Markovic", "vule97+petar@gmail.com", "065 353 97 23");
+        userService.registerUser(dto);
+    }
+
+    @Test(expected = EmailInUseException.class)
+    public void registerEmailInUse() {
+        UserDTO dto = new UserDTO("perica", "test123", "test123", "Petar", "Markovic", "vule97@gmail.com", "065 353 97 23");
+        boolean response = userService.registerUser(dto);
+    }
+
     @Test
     public void verify() {
         boolean response = userService.verifyAccount("vule97+petar@gmail.com", "de4939e0-df55-4289-a7e8-8009309680df");
         assertTrue(response);
+    }
+
+    @Test(expected = FieldsRequiredException.class)
+    public void verifyNoData() {
+        userService.verifyAccount("", null);
+    }
+
+    @Test(expected = WrongVerificationTokenAndEmail.class)
+    public void verifyWrongData() {
+        userService.verifyAccount("vule97+petar@gmail.com", "neki token");
     }
 }
--- KTS_NVT/src/test/java/Tim20/KTS_NVT/service/UserServiceUnitTest.java
+++ KTS_NVT/src/test/java/Tim20/KTS_NVT/service/UserServiceUnitTest.java
@@ -9,18 +9,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import tim20.KTS_NVT.dto.UserDTO;
-import tim20.KTS_NVT.model.SeatsSector;
-import tim20.KTS_NVT.model.Sector;
-import tim20.KTS_NVT.model.StandSector;
+import tim20.KTS_NVT.exceptions.*;
 import tim20.KTS_NVT.model.User;
-import tim20.KTS_NVT.repository.SectorRepository;
+import tim20.KTS_NVT.model.UserRole;
 import tim20.KTS_NVT.repository.UserRepository;
 import tim20.KTS_NVT.security.UserTokenState;
 import tim20.KTS_NVT.service.UserService;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertTrue;
@@ -37,9 +33,14 @@ public class UserServiceUnitTest {
 
     @Before
     public void setUp() {
-        User user = new User(1l, "perica", "test123", "Petar", "Markovic", "vule97+petar@gmail.com", "065 353 97 23", true, "verifToken", null);
+        UserRole adminRole = new UserRole(1l, "ADMIN");
+        Set<UserRole> userRoles = new HashSet<>(Arrays.asList(adminRole));
+        User user = new User(1l, "perica", "test123", "Petar", "Markovic", "vule97+petar@gmail.com", "065 353 97 23", true, "de4939e0-df55-4289-a7e8-8009309680df", userRoles);
         Optional<User> optionalUser = Optional.of(user);
         Mockito.when(userRepositoryMocked.findById(user.getId())).thenReturn(optionalUser);
+        Mockito.when(userRepositoryMocked.findByUsername(user.getUsername())).thenReturn(user);
+        Mockito.when(userRepositoryMocked.findByEmail(user.getEmail())).thenReturn(user);
+        Mockito.when(userRepositoryMocked.findByEmailAndVerificationToken(user.getEmail(), user.getVerificationToken())).thenReturn(user);
 
         List<User> users = new ArrayList<>();
         users.add(user);
@@ -73,13 +74,13 @@ public class UserServiceUnitTest {
 
     @Test
     public void login() {
-        UserDTO dto = new UserDTO("vpetrovic", "test123");
+        UserDTO dto = new UserDTO("perica", "test123");
         UserTokenState token = userService.loginUser(dto);
 
         assertNotNull(token);
     }
 
-    @Test
+    @Test(expected = WrongCredentialsException.class)
     public void loginWrongCredentials() {
         UserDTO dto = new UserDTO("perke", "asdfasd");
         UserTokenState token = userService.loginUser(dto);
@@ -87,18 +88,60 @@ public class UserServiceUnitTest {
         assertNull(token);
     }
 
+    @Test(expected = FieldsRequiredException.class)
+    public void loginNoData() {
+        UserDTO dto = new UserDTO(null, "");
+        UserTokenState token = userService.loginUser(dto);
+
+        assertNotNull(token);
+    }
+
     @Test
     public void register() {
-        UserDTO dto = new UserDTO("perica", "test123", "test123",
-                "Petar", "Markovic", "vule97+petar@gmail.com", "065 353 97 23");
+        UserDTO dto = new UserDTO("perica2", "test123", "test123",
+                "Petar", "Markovic", "vule97+petar2@gmail.com", "065 353 97 23");
 
         boolean response = userService.registerUser(dto);
         assertTrue(response);
+    }
+
+    @Test(expected = FieldsRequiredException.class)
+    public void registerNoData() {
+        UserDTO dto = new UserDTO("", "", "", "", "", "", "");
+        userService.registerUser(dto);
+    }
+
+    @Test(expected = UsernameTakenException.class)
+    public void registerUsernameTaken() {
+        UserDTO dto = new UserDTO("perica", "test123", "test123", "Petar", "Markovic", "perica@gmail.com", "065 353 97 23");
+        userService.registerUser(dto);
+    }
+
+    @Test(expected = PasswordsNotMatchingException.class)
+    public void registerPasswordsDontMatch() {
+        UserDTO dto = new UserDTO("perica2", "test123", "Test123", "Petar", "Markovic", "vule97+petar2@gmail.com", "065 353 97 23");
+        userService.registerUser(dto);
+    }
+
+    @Test(expected = EmailInUseException.class)
+    public void registerEmailInUse() {
+        UserDTO dto = new UserDTO("perica2", "test123", "test123", "Petar", "Markovic", "vule97+petar@gmail.com", "065 353 97 23");
+        boolean response = userService.registerUser(dto);
     }
 
     @Test
     public void verify() {
         boolean response = userService.verifyAccount("vule97+petar@gmail.com", "de4939e0-df55-4289-a7e8-8009309680df");
         assertTrue(response);
+    }
+
+    @Test(expected = FieldsRequiredException.class)
+    public void verifyNoData() {
+        userService.verifyAccount("", null);
+    }
+
+    @Test(expected = WrongVerificationTokenAndEmail.class)
+    public void verifyWrongData() {
+        userService.verifyAccount("vule97+petar@gmail.com", "neki token");
     }
 }
